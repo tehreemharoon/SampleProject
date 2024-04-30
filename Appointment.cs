7f62a95d5ab81bc0297457b734b12128b9d953d2
Tehreem Haroon <tehreem.haroon@marquette.edu>
2024-04-30 14:16:34 -0500

    added appointment class

---
+++ Appointment.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleProject;
+
+public class Appointment
+{
+    private static int autoIncrement; 
+    public int Id {get;}
+    public DateTime dateTime {get;set;}
+    public Appointment ()
+    {
+        autoIncrement++ ;
+        Id = autoIncrement ;
+    }
+}
