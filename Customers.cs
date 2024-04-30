7754e32d18f2e5b793fd87c7dae842f3adebb281
Tehreem Haroon <tehreem.haroon@marquette.edu>
2024-04-30 14:25:27 -0500

    added customers class for authntication

---
+++ Customers.cs
@@ -0,0 +1,22 @@
+﻿namespace SampleProject;
+
+public class Customers
+{
+    public List<Customer> customerList { get; set; }
+    public Customers ()
+    {
+        customerList = new List<Customer> ();
+    }
+    public Customer Authenticate (string username, string password)
+    {
+        var c = customerList.Where(o => (o.Username == username) && ( o.Password == password));
+        if (c.Count () > 0)
+        {
+            return c.First ();
+        }
+        else 
+        {
+            return null;
+        }
+    }
+}
